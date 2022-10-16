d06502316a1c4b62d9552a6b984cd2979c8f2b3c
danish0786000 <mddanishalauddin@gmail.com>
2022-10-16 12:37:41 +0530

    Menu driven crud operation using spring orm

--- src/main/java/com/spring/orm/App.java
+++ src/main/java/com/spring/orm/App.java
@@ -1,5 +1,8 @@
 package com.spring.orm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -16,8 +19,48 @@ public class App
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
         StudentDao studentDao=context.getBean("studentDao",StudentDao.class);
-        Student s=new Student(111,"danish","muzaffarpur");
-        int r=studentDao.insert(s);
-        System.out.println(r +" line added");
+//        Student s=new Student(111,"danish","muzaffarpur");
+//        int r=studentDao.insert(s);
+//        System.out.println(r +" line added");
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Press 1 for add new student");
+        System.out.println("press 2 for display all student");
+        System.out.println("press 3 for get detail of single student");
+        System.out.println("press 4 for delete student");
+        System.out.println("press 5 for update student");
+        System.out.println("press 6 for exit");
+        boolean go=true;
+        while(go)
+        {
+        	try {
+        		int choice=Integer.parseInt(br.readLine());
+        		
+        		switch (choice) {
+				case 1:
+					//add new student
+					break;
+				case 2:
+					//display all stuent
+					break;
+				case 3:
+					//display one student
+					break;
+				case 4:
+					//delete student
+					break;
+				case 5:
+					//update student
+					break;
+				
+
+				case 6:
+					//exit
+					go=false;
+					break;
+        		}
+			} catch (Exception e) {
+				System.out.println("invalid input try another input");
+			}
+        }
     }
 }
--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.orm.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -14,6 +16,33 @@ public class StudentDao {
 		//insert
 		Integer i =(Integer) this.temp.save(student);
 		return i;
+	}
+	
+	//get the single object
+	public Student getStudent(int studentId)
+	{
+		Student student =this.temp.get(Student.class, studentId);
+		return student;
+	}
+	//get all rows
+	public List<Student> getAllStudent()
+	{
+		List<Student> students=this.temp.loadAll(Student.class);
+		return students;
+	}
+	//delete student
+	@Transactional
+	public void deleteStudent(int studentId)
+	{
+		Student student=this.temp.get(Student.class, studentId);
+		this.temp.delete(student);
+		
+	}
+	//update student
+	@Transactional
+	public void updateStudent(Student student)
+	{
+		this.temp.update(student);
 	}
 	public HibernateTemplate getTemp() {
 		return temp;
