d9f956329c558341d902b63529aeae81fc5e9bf5
danish0786000 <mddanishalauddin@gmail.com>
2022-10-16 17:39:34 +0530

    Complete Console Based Student Deatils Management System Using SpringOrm which perform CRUD operation

--- src/main/java/com/spring/orm/App.java
+++ src/main/java/com/spring/orm/App.java
@@ -2,6 +2,7 @@ package com.spring.orm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -38,18 +39,72 @@ public class App
         		switch (choice) {
 				case 1:
 					//add new student
+					System.out.println("Enter student id");
+					int id=Integer.parseInt(br.readLine());
+					
+					System.out.println("Enter Student name");
+					String name=br.readLine();
+					
+					System.out.println("Enter student city");
+					String city=br.readLine();
+					
+					Student s=new Student();
+					s.setId(id);
+					s.setName(name);
+					s.setCity(city);
+					
+					int r=studentDao.insert(s);
+					System.out.println(r+" student added");
+					System.out.println("=============");
+					System.out.println();
 					break;
 				case 2:
 					//display all stuent
+					List<Student> allStudents=studentDao.getAllStudent();
+					System.out.println("=================");
+					for (Student student : allStudents) {
+						System.out.println("id "+ student.getId());
+						System.out.println("Name "+ student.getName());
+						System.out.println("City"+ student.getCity());
+						System.out.println("---------------");
+					}
+					System.out.println("===================");
 					break;
 				case 3:
 					//display one student
+					System.out.println("enter student id");
+					int id1=Integer.parseInt(br.readLine());
+					Student student=studentDao.getStudent(id1);
+					System.out.println("id "+ student.getId());
+					System.out.println("Name "+ student.getName());
+					System.out.println("City"+ student.getCity());
+					System.out.println("---------------");
 					break;
 				case 4:
 					//delete student
+					System.out.println("enter student id");
+					int id11=Integer.parseInt(br.readLine());
+					studentDao.deleteStudent(id11);
+					System.out.println("student deleted");
 					break;
 				case 5:
 					//update student
+					System.out.println("Enter student id");
+					int idd=Integer.parseInt(br.readLine());
+					
+					System.out.println("Enter Student name");
+					String namee=br.readLine();
+					
+					System.out.println("Enter student city");
+					String cityy=br.readLine();
+					
+					Student ss=new Student();
+					ss.setId(idd);
+					ss.setName(namee);
+					ss.setCity(cityy);
+					studentDao.updateStudent(ss);
+					System.out.println("student details updated");
+					
 					break;
 				
 
